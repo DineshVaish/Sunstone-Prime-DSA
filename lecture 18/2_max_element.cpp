e90ec17c07e63c1b2b9f3222ca0d65fa29dfb390
Dineshvaish <dineshkumarchaudhari410@gmail.com>
2026-02-10 18:13:57 +0530

    vector

--- lecture 18/2_max_element.cpp
+++ lecture 18/2_max_element.cpp
@@ -1,26 +1,20 @@
-#include<iostream>
+#include <iostream>
 #include<climits>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
-    cin>>n
+    cin>>n;
     int arr[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     int ans=INT_MIN;
-    for(int i=0;i<n;i++)
-    {
-        if(ans <= arr[i])
-        {
+    for(int i=0;i<n;i++){
+        if(ans<arr[i]){
             ans=arr[i];
         }
     }
-
-
-    cout<<ans;
+    cout<<ans<<endl;
     return 0;
 }
